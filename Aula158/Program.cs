de6e70d18937a1247b8cac3e2fe4ef1266ab29ee
GRUPPY02 <GRUPPY02@DESKTOP-B3K8HS6>
2025-03-31 17:58:12 -0300

    Asasasdasas

---
+++ Aula158/Program.cs
@@ -0,0 +1,33 @@
+﻿Dictionary<string, int> dictionary = new Dictionary<string, int>();
+
+string sourcePathFile = Console.ReadLine();
+
+try
+{
+    using StreamReader sr = File.OpenText(sourcePathFile);
+    while (!sr.EndOfStream)
+    {
+        string[] line = sr.ReadLine().Split(',');
+        string candidate = line[0];
+        int votes = int.Parse(line[1]);
+        dictionary.Add(candidate, votes);
+
+        if (dictionary.ContainsKey(candidate))
+        {
+            dictionary[candidate] += votes;
+        }
+        else
+        {
+            dictionary[candidate] = votes;
+        }
+    }
+    foreach (var item in dictionary)
+    {
+        Console.WriteLine(item.Key + ": " + item.Value);
+    }
+
+}
+catch(Exception e)
+{
+    Console.WriteLine(e.Message);
+}
